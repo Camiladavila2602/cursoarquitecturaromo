3e01b4ccc383cd339fac7683f23cef9f46758229
Camiladavila2602 <camiladavila2602@gmail.com>
2024-12-13 14:03:36 -0500

    Creacion northwind.consoleapp

---
+++ Northwind/NorthWind/NorthWind.Entities/Interfaces/IUserActionWriter.cs
@@ -0,0 +1,13 @@
+﻿using NorthWind.Entities.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace NorthWind.Entities.Interfaces
+{
+    public interface IUserActionWriter
+    {
+        void Write(UserAction action);
+    }
+}
